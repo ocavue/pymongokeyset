b579cd164911b623e0994c33ea7f32eba1a27bb4
ocavue <ocavue@gmail.com>
2017-09-04 14:03:12 +0800

    comment

--- pymongokeyset/utils.py
+++ pymongokeyset/utils.py
@@ -1,5 +1,10 @@
 
 def get_key(d, key):
+    '''
+    >>> get_key({'a': {'b': {'c': 1}}}, 'a.b.c')
+    1
+    '''
+
     result = d
     for k in key.split('.'):
         if isinstance(result, dict):
