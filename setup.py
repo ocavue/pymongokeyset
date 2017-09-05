5ba80d16f6ba63350ef21bec11acc4d02620aea4
ocavue <ocavue@gmail.com>
2017-09-05 10:31:05 +0800

    0.0.8

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pymongokeyset',
-    version='0.0.7',
+    version='0.0.8',
     keywords=('keyset', 'pymongo'),
     description='offset-free paging for pymongo',
     author='ocavue',
