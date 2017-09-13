35053060e18bee44ef5922b33ed94520467fc9ee
ocavue <ocavue@gmail.com>
2017-09-14 02:23:10 +0800

    add a test case

--- tests/new.py
+++ tests/new.py
@@ -19,7 +19,7 @@ class NormalTestCase(BaseTestCase):
         {'m': 1, 'n': 1, '_id': 7},
     ]  # yapf: disable
 
-    def test_normal_case(self):
+    def test_single_sort_case(self):
         cursor1 = get_page(self.collect, limit=3, sort=[('_id', 1)])
         self.assertEqual(list(cursor1), self.objs[:3])
         self.assertTrue(cursor1.paging.has_next)
@@ -41,6 +41,38 @@ class NormalTestCase(BaseTestCase):
         self.assertFalse(hasattr(cursor2.paging, 'has_next'))
 
         cursor1 = get_page(self.collect, limit=3, sort=[('_id', 1)], position=cursor2.paging.previous_position)
+        self.assertEqual(list(cursor1), self.objs[:3])
+        self.assertFalse(cursor1.paging.has_previous)
+        self.assertFalse(hasattr(cursor1.paging, 'has_next'))
+
+    def test_mulit_sort_case(self):
+        search_condictions = {
+            'collection': self.collect,
+            'limit': 3,
+            'sort': [('m', 1), ('n', 1), ('_id', 1)],
+        }
+
+        cursor1 = get_page(**search_condictions)
+        self.assertEqual(list(cursor1), self.objs[:3])
+        self.assertTrue(cursor1.paging.has_next)
+        self.assertFalse(hasattr(cursor1.paging, 'has_previous'))
+
+        cursor2 = get_page(**search_condictions, position=cursor1.paging.next_position)
+        self.assertEqual(list(cursor2), self.objs[3:6])
+        self.assertTrue(cursor2.paging.has_next)
+        self.assertFalse(hasattr(cursor2.paging, 'has_previous'))
+
+        cursor3 = get_page(**search_condictions, position=cursor2.paging.next_position)
+        self.assertEqual(list(cursor3), self.objs[6:])
+        self.assertFalse(cursor3.paging.has_next)
+        self.assertFalse(hasattr(cursor3.paging, 'has_previous'))
+
+        cursor2 = get_page(**search_condictions, position=cursor3.paging.previous_position)
+        self.assertEqual(list(cursor2), self.objs[3:6])
+        self.assertTrue(cursor2.paging.has_previous)
+        self.assertFalse(hasattr(cursor2.paging, 'has_next'))
+
+        cursor1 = get_page(**search_condictions, position=cursor2.paging.previous_position)
         self.assertEqual(list(cursor1), self.objs[:3])
         self.assertFalse(cursor1.paging.has_previous)
         self.assertFalse(hasattr(cursor1.paging, 'has_next'))
