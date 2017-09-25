c0d04b3671fed7eb7349927ca1358a2167cf5f2f
ocavue <ocavue@gmail.com>
2017-09-25 11:58:56 +0800

    args name

--- tests/spec_filter.py
+++ tests/spec_filter.py
@@ -12,34 +12,34 @@ class SpecTestCase(BaseTestCase):
     } for i in range(100)]
 
     def test_next(self):
-        page1 = get_page(collection=self.collect, sort=[['a.b', 1]], limit=20)
-        self.assertEqual([i['a']['b'] for i in page1], list(range(0, 20)))
+        cursor1 = get_page(collection=self.collect, sort=[['a.b', 1]], limit=20)
+        self.assertEqual([i['a']['b'] for i in cursor1], list(range(0, 20)))
 
-        page2 = get_page(collection=self.collect, sort=[['a.b', 1]], limit=20, position=page1.paging.next_position)
-        self.assertEqual([i['a']['b'] for i in page2], list(range(20, 40)))
+        cursor2 = get_page(collection=self.collect, sort=[['a.b', 1]], limit=20, position=cursor1.paging.next_position)
+        self.assertEqual([i['a']['b'] for i in cursor2], list(range(20, 40)))
 
-        page1 = get_page(collection=self.collect, sort=[['a.b', 1]], limit=20, position=page2.paging.previous_position)
-        self.assertEqual([i['a']['b'] for i in page1], list(range(0, 20)))
+        cursor1 = get_page(collection=self.collect, sort=[['a.b', 1]], limit=20, position=cursor2.paging.previous_position)
+        self.assertEqual([i['a']['b'] for i in cursor1], list(range(0, 20)))
 
 
 class SortTestCase(BaseTestCase):
     objs = [{'a': i} for i in range(10)]
 
     def test_asc_num(self):
-        page1 = get_page(sort=[['a', 1]], limit=3, collection=self.collect)
-        self.assertEqual([i['a'] for i in page1], [0, 1, 2])
+        cursor = get_page(sort=[['a', 1]], limit=3, collection=self.collect)
+        self.assertEqual([i['a'] for i in cursor], [0, 1, 2])
 
     def test_desc_num(self):
-        page1 = get_page(sort=[['a', -1]], limit=3, collection=self.collect)
-        self.assertEqual([i['a'] for i in page1], [9, 8, 7])
+        cursor = get_page(sort=[['a', -1]], limit=3, collection=self.collect)
+        self.assertEqual([i['a'] for i in cursor], [9, 8, 7])
 
     def test_asc_arg(self):
-        page1 = get_page(sort=[['a', ASCENDING]], limit=3, collection=self.collect)
-        self.assertEqual([i['a'] for i in page1], [0, 1, 2])
+        cursor = get_page(sort=[['a', ASCENDING]], limit=3, collection=self.collect)
+        self.assertEqual([i['a'] for i in cursor], [0, 1, 2])
 
     def test_desc_arg(self):
-        page1 = get_page(sort=[['a', DESCENDING]], limit=3, collection=self.collect)
-        self.assertEqual([i['a'] for i in page1], [9, 8, 7])
+        cursor = get_page(sort=[['a', DESCENDING]], limit=3, collection=self.collect)
+        self.assertEqual([i['a'] for i in cursor], [9, 8, 7])
 
 
 class FilterTestCase(BaseTestCase):
@@ -48,20 +48,20 @@ class FilterTestCase(BaseTestCase):
     def test_single_filter(self):
         params = dict(filter={'b': 0}, limit=2, sort=[['a', 1]], collection=self.collect)
 
-        page1 = get_page(**params)
-        self.assertEqual([i['a'] for i in page1], [0, 2])
+        cursor1 = get_page(**params)
+        self.assertEqual([i['a'] for i in cursor1], [0, 2])
 
-        page2 = get_page(**params, position=page1.paging.next_position)
-        self.assertEqual([i['a'] for i in page2], [4, 6])
+        cursor2 = get_page(**params, position=cursor1.paging.next_position)
+        self.assertEqual([i['a'] for i in cursor2], [4, 6])
 
     def test_mulit_filter(self):
         params = dict(filter={'a': {'$gt': 0}, 'b': 0}, limit=2, sort=[['a', 1]], collection=self.collect)
 
-        page1 = get_page(**params)
-        self.assertEqual([i['a'] for i in page1], [2, 4])
+        cursor1 = get_page(**params)
+        self.assertEqual([i['a'] for i in cursor1], [2, 4])
 
-        page2 = get_page(**params, position=page1.paging.next_position)
-        self.assertEqual([i['a'] for i in page2], [6, 8])
+        cursor2 = get_page(**params, position=cursor1.paging.next_position)
+        self.assertEqual([i['a'] for i in cursor2], [6, 8])
 
 
 class ProjectionTestCase(BaseTestCase):
