33f59ca999771e45cec573c425028b868c09f45b
ocavue <ocavue@gmail.com>
2017-09-25 11:58:48 +0800

    runnable example

--- example/simple_example.py
+++ example/simple_example.py
@@ -1,29 +1,36 @@
 from prepare import prepare
-from pymongokeyset import get_page
+from pymongokeyset import get_keyset_cursor
 
 
 def main():
     collection = prepare()
     collection.insert_many([{'_id': i} for i in range(7)])
 
+    search_condictions = {
+        'collection': collection,   
+        'filter': {},
+        'sort': [('_id', 1)],
+        'limit': 5,
+    }
+
     # gets the first page
-    page1 = get_page(collection.find({}).sort([('_id', 1)]), limit=5)
-    print('page1:', page1)  # [{'_id': 0}, {'_id': 1}, {'_id': 2}, {'_id': 3}, {'_id': 4}]
+    cursor1 = get_keyset_cursor(**search_condictions)
+    print('page1:', list(cursor1))  # [{'_id': 0}, {'_id': 1}, {'_id': 2}, {'_id': 3}, {'_id': 4}]
 
     # gets the second page
-    page2 = get_page(collection.find({}).sort([('_id', 1)]), limit=5, position=page1.paging.next_position)
-    print('page2:', page2)  # [{'_id': 5}, {'_id': 6}]
+    cursor2 = get_keyset_cursor(**search_condictions, position=cursor1.paging.next_position)
+    print('page2:', list(cursor2))  # [{'_id': 5}, {'_id': 6}]
 
     collection.insert({'_id': -1})
 
     # the first page again, backwards from the previous page
-    page1 = get_page(collection.find({}).sort([('_id', 1)]), limit=5, position=page2.paging.previous_position)
-    print('page1:', page1)  # [{'_id': 0}, {'_id': 1}, {'_id': 2}, {'_id': 3}, {'_id': 4}]
+    cursor1 = get_keyset_cursor(**search_condictions, position=cursor2.paging.previous_position)
+    print('page1:', list(cursor1))  # [{'_id': 0}, {'_id': 1}, {'_id': 2}, {'_id': 3}, {'_id': 4}]
 
     # what if new items were added at the start?
-    if page1.paging.has_previous:
-        page0 = get_page(collection.find({}).sort([('_id', 1)]), limit=5, position=page1.paging.previous_position)
-        print('page0:', page0)  # [{'_id': -1}]
+    if cursor1.paging.has_previous:
+        cursor0 = get_keyset_cursor(**search_condictions, position=cursor1.paging.previous_position)
+        print('page0:', list(cursor0))  # [{'_id': -1}]
 
 
 if __name__ == '__main__':
